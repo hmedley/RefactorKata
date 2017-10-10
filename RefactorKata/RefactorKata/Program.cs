39d34c9d0dc407d843f2bd746259ecd5f5c13fcc
Hollis Medley <hmedley00@gmail.com>
2017-10-10 13:55:23 -0500

    Made changes to the public class 'product' and shotened the 'get set'. created 'using System.DataLinq' and converted all of the variable names to variables. Finally I changed the 'main string' to a private static class.

--- RefactorKata/RefactorKata/Program.cs
+++ RefactorKata/RefactorKata/Program.cs
@@ -1,41 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace RefactorKata
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
             //This is intentionally bad : (  Let's Refactor!
-            System.Data.SqlClient.SqlConnection Conn = new System.Data.SqlClient.SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
+            var conn = new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
 
-            System.Data.SqlClient.SqlCommand cmd = Conn.CreateCommand();
+            var cmd = conn.CreateCommand();
             cmd.CommandText = "select * from Products";
             /*
              * cmd.CommandText = "Select * from Invoices";
              */
-            System.Data.SqlClient.SqlDataReader reader = cmd.ExecuteReader();
-            List<Product> products = new List<Product>();
+            var reader = cmd.ExecuteReader();
+            var products = new List<Product>();
 
             //TODO: Replace with Dapper
             while (reader.Read())
             {
-                var prod = new Product();
-                prod.name = reader["Name"].ToString();
+                var prod = new Product {Name = reader["Name"].ToString()};
                 products.Add(prod);
             }
-            Conn.Dispose();
+            conn.Dispose();
             Console.WriteLine("Products Loaded!");
-            for (int i =0; i< products.Count; i++)
+            foreach(var product in products)
             {
-                Console.WriteLine(products[i].name);
+                
+            }
+            {
+                Console.WriteLine(products.Count);
             }
         }
     }
     public class Product
     {
-        public string name;
-        public string Name { get { return name; } set { name = value; } }
+        public string Name { get; set; }
     }
 }
