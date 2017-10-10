5891695fdc060c0d768b5ef1074d41c606f702ee
Hollis Medley <hmedley00@gmail.com>
2017-10-10 14:20:35 -0500

    changed for statement to foreach. created a 'using var conn class?' created ienumerable

--- RefactorKata/RefactorKata/Program.cs
+++ RefactorKata/RefactorKata/Program.cs
@@ -1,43 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
 
 namespace RefactorKata
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            //This is intentionally bad : (  Let's Refactor!
-            var conn = new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
+            var products = GetProducts();
+            \
+            private static IEnumerable<Product> GetProducts()
+        {
+            using (var conn =
+                new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;"))
 
-            var cmd = conn.CreateCommand();
-            cmd.CommandText = "select * from Products";
-            /*
-             * cmd.CommandText = "Select * from Invoices";
-             */
-            var reader = cmd.ExecuteReader();
-            var products = new List<Product>();
+            {
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "select * from Products";
+                /*
+                 * cmd.CommandText = "Select * from Invoices";
+                 */
+                var reader = cmd.ExecuteReader();
+                var products = new List<Product>();
 
-            //TODO: Replace with Dapper
-            while (reader.Read())
-            {
-                var prod = new Product {Name = reader["Name"].ToString()};
-                products.Add(prod);
-            }
-            conn.Dispose();
-            Console.WriteLine("Products Loaded!");
-            foreach(var product in products)
-            {
-                
-            }
-            {
-                Console.WriteLine(products.Count);
+                //TODO: Replace with Dapper
+                while (reader.Read())
+                {
+                    var prod = new Product {Name = reader["Name"].ToString()};
+                    products.Add(prod);
+                }
+                conn.Dispose();
+                Console.WriteLine("Products Loaded!");
+                foreach (var product in products)
+                {
+                    Console.WriteLine(products.Count);
+                }
             }
         }
-    }
-    public class Product
-    {
-        public string Name { get; set; }
+
+        public class Product
+        {
+            public string Name { get; set; }
+        }
     }
 }
